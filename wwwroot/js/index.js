540dcea690c28af799552cf8d70ec7f66311ed04
Mark Wetzel <mark@markwetzel.com>
2020-10-20 11:55:36 -0400

    End of section 4.

---
+++ wwwroot/js/index.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+	console.log("Hello");
+
+	const theForm = $("#the-form");
+	theForm.hide();
+
+	const button = $("#buy-button");
+	button.on("click",
+		() => {
+			console.log("Buying Item");
+		});
+
+	const productInfo = $(".product-props li");
+	productInfo.on('click',
+		function () {
+			console.log("You clicked on " + $(this).text());
+		});
+
+	const $loginToggle = $('#login-toggle');
+	const $popupForm = $('.popup-form');
+	$loginToggle.on('click', function () {
+		$popupForm.fadeToggle(1000);
+	})
+});
