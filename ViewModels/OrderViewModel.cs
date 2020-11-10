c3d5e5bb8685848d4086bf0b5ef01c7511523a88
Mark Wetzel <mark@markwetzel.com>
2020-11-09 19:17:03 -0500

    8

--- ViewModels/OrderViewModel.cs
+++ ViewModels/OrderViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DutchTreat.ViewModels {
@@ -9,5 +10,7 @@ namespace DutchTreat.ViewModels {
         [Required]
         [MinLength (4)]
         public string OrderNumber { get; set; }
+
+        public ICollection<OrderItemViewModel> Items { get; set; }
     }
 }
